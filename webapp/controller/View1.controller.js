6c8539f43d87b35cc8d2c0dcce51bb3119933311
Nagaraju N <nagaraju@nzseasonalwear.com>
2026-06-26 08:21:38 +0000

    initialize project

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("zpp413e.controller.View1", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zpp413e/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
